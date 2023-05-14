dee45a65acb782c652fd1523faf12b44ae78b759
KYPratiksha <133528277+KYPratiksha@users.noreply.github.com>
2023-05-14 22:56:51 +0530

    Delete CWHtut30_seek_tell.py

--- CWHtut30_seek_tell.py
+++
@@ -1,9 +0,0 @@
-f = open("Harry.txt")
-
-print(f.tell())       #shows at which character file pointer is
-print(f.readline())
-print(f.tell())
-f.seek(0)
-print(f.readline())
-print(f.tell())
-f.close() 
