371d3cbe01d516bdeb6805b45b0122efc98dec65
KYPratiksha <133528277+KYPratiksha@users.noreply.github.com>
2023-05-24 16:35:01 +0530

    Delete CWHtut74_forelseloop.py

--- CWHtut74_forelseloop.py
+++
@@ -1,9 +0,0 @@
-lit = ["roti", "sabzi","daal ", "chaawal"]
-for item in lit:
-    if item == "roll":
-        break
-else:
-    print("found")
-
-
-    # else can be used with for only when the conditions of fro loop is satisfied
